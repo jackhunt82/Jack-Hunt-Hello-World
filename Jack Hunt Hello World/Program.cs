d0ae63093991bb558ace174126ce3f59c92dadeb
jackh <jackh@DESKTOP-1MQDR5N>
2020-09-07 07:06:01 -0500

    version 3 - Hello World

--- Jack Hunt Hello World/Program.cs
+++ Jack Hunt Hello World/Program.cs
@@ -10,6 +10,10 @@ namespace Jack_Hunt_Hello_World
     {
         static void Main(string[] args)
         {
+            //Pause to display Hello World!
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
